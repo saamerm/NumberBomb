01026fdf89f918686c7758bd2f71aeb60c5a2158
Fads09 <faitamrou.90@gmail.com>
2021-02-17 01:29:19 -0500

    features/11-GTP-PreferencesStorage
    *  Used Xamarin.Essentials to store the input value (GamerTag) in the preferences storage
    
    **Testing**
    -------------------------------------
    
    iOS > LaunchScreen > GameTagPage > Enter a name > Click Enter > Exit the app > Reopen the app > the name is saved
    
    Android > LaunchScreen > GameTagPage > Enter a name > Click Enter > Exit the app > Reopen the app > the name is saved

--- NumberBomb/NumberBomb/MainPage.xaml.cs
+++ NumberBomb/NumberBomb/MainPage.xaml.cs
@@ -5,14 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace NumberBomb
 {
     public partial class MainPage : ContentPage
-    {
+    { 
         public MainPage()
         {
             InitializeComponent();
+            NameEntry.Text = Preferences.Get("NameTag", string.Empty);
+        }
+
+        public async void OnButtonClicked(object sender, EventArgs e)
+        {
+            Preferences.Set("NameTag", NameEntry.Text);
         }
     }
 }
