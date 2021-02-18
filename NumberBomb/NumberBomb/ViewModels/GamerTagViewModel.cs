2870eddc53211325cc7e9eae92750cf82ed858d3
Fads09 <faitamrou.90@gmail.com>
2021-02-18 18:53:21 -0500

    features/13-GTPage-ViewModel
    * Created a GamerTagViewModel
    * Binded GamerTagViewModel  to the MainPage
    * Implemented INotifyPropertyChanged to keep track of the Entry changes
    * Used Xamarin.Essentials to store the input value (GamerTag) in the preferences storage
    
    **Testing**
    ----------------------------------
    
    * iOS > LaunchScreen > GamerTagPage > Entered the GamerTag  in the Entry Field > Clicked the Enter button > then removed the value > Relaunched the app > the GamerTag is still saved
    * Android > LaunchScreen > GamerTagPage > Entered a value in the Entry Field > Clicked the Enter button > then removed the value > Relaunched the app > the value is still saved

---
+++ NumberBomb/NumberBomb/ViewModels/GamerTagViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace NumberBomb.ViewModels
+{
+    public class GamerTagViewModel : INotifyPropertyChanged
+    {
+        public string _gamerTag;
+        public ICommand ButtonCommand { get; set; }
+        public string GamerTag {
+            get
+            {
+                return _gamerTag;
+            }
+            set
+            {
+                _gamerTag = value;
+                if(PropertyChanged != null)                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(GamerTag)));
+                }
+            }
+        }
+
+        public GamerTagViewModel()
+        {
+            GamerTag = Preferences.Get("NameTag", string.Empty);
+            ButtonCommand = new Command(ButtonCommandExecute);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void ButtonCommandExecute(object obj)
+        {
+            Preferences.Set("NameTag", GamerTag);
+        }
+    }
+}
