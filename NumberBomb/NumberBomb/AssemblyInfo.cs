00c0aaad44a489f7a2e202520077b7637f7b84f4
Fadoua Amrou <faitamrou.90@gmail.com>
2021-02-24 15:19:47 -0500

    features/25-HomePage-Title (#28)
    * Created User Icon
    * Created Info Icon
    
    **Testing**
    -----------------------------------
    
    * iOS > LaunchScreen > HomePage > Icons are showing
    * iOS > LaunchScreen > GamerTagPage > Enter Name > HomePage > Icons are showing
    * Android > LaunchScreen > HomePage > Icons are showing
    * Android > LaunchScreen > GamerTagPage > Enter Name > HomePage > Icons are showing

--- NumberBomb/NumberBomb/AssemblyInfo.cs
+++ NumberBomb/NumberBomb/AssemblyInfo.cs
@@ -3,3 +3,5 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly:ExportFont("PatrickHand-Regular.ttf", Alias ="PatrickFont")]
+[assembly: ExportFont("FontAwesome5Regular.otf", Alias = "FontAwesome5Regular")]
+[assembly: ExportFont("FontAwesome5Solid.otf", Alias = "FontAwesome5Solid")]
