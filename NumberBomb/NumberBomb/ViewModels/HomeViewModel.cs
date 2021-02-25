780a4ebe5ab42fe740b07aad0c7d96ca41743e55
Fadoua Amrou <faitamrou.90@gmail.com>
2021-02-25 15:25:55 -0500

    features/29-HomePage-ViewModel (#31)
    * Setup the ViewModel so that the pages are opened from the ViewModel Commands
    
    **Testing**
    --------------------------------------
    
    * iOS > LaunchScreen > GamerTagPage > Enter Name >
       HomePage > Click Start button > GamePage
    
    * iOS > LaunchScreen > GamerTagPage > Enter Name >
       HomePage > Click LeaderBoard button > LeaderBoardPage
    
    * iOS > LaunchScreen > GamerTagPage > Enter Name >
       HomePage > Click Info Icon > InfoPage
    
    * iOS > LaunchScreen > GamerTagPage > Enter Name >
       HomePage > Click User Icon> ChangeUserPage
    
    * Android > LaunchScreen > GamerTagPage > Enter Name >
       HomePage > Click Start button > GamePage
    
    * Android > LaunchScreen > GamerTagPage > Enter Name >
       HomePage > Click LeaderBoard button > LeaderBoardPage
    
    * Android > LaunchScreen > GamerTagPage > Enter Name >
       HomePage > Click Info Icon > InfoPage
    
    * Android > LaunchScreen > GamerTagPage > Enter Name >
       HomePage > Click User Icon> ChangeUserPage

--- NumberBomb/NumberBomb/ViewModels/HomeViewModel.cs
+++ NumberBomb/NumberBomb/ViewModels/HomeViewModel.cs
@@ -1,10 +1,42 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 namespace NumberBomb.ViewModels
 {
     public class HomeViewModel
     {
+        public ICommand StartButtonCommand { get; set; }
+        public ICommand LeaderBoardButtonCommand { get; set; }
+        public ICommand UserIcon_OnTapped { get; set; }
+        public ICommand InfoIcon_OnTapped { get; set; }
+
         public HomeViewModel()
         {
+            StartButtonCommand = new Command(StartCommandExecute);
+            LeaderBoardButtonCommand = new Command(LeaderBoardCommandExecute);
+            UserIcon_OnTapped = new Command(UserIconCommandExecute);
+            InfoIcon_OnTapped = new Command(InfoIconCommandExecute);  
         }
+
+        private void UserIconCommandExecute(object obj)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new ChangeUserPage());
+        }
+
+        private void InfoIconCommandExecute(object obj)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new InfoPage());
+        }
+
+        private void LeaderBoardCommandExecute(object obj)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new LeaderboardPage());
+        }
+
+        private void StartCommandExecute(object obj)
+        {
+            App.Current.MainPage.Navigation.PushAsync(new GamePage());
+        }     
     }
 }
