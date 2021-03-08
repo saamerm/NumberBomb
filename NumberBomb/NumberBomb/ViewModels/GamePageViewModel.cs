ab76b4a797edd0d6fcf12f410137a6bed1c8e8bd
Fadoua Amrou <faitamrou.90@gmail.com>
2021-03-08 14:12:40 -0500

    features/34-GamePage-ViewModelNWin (#46)
    * Made the entry accept only numbers
    * Created a GamePageViewModel binded to the Page
    * The ViewModel contains the RandomNumber int property that contains a random number from 1-100 that will be stored for the entire game session
    * Created a GuessedValue int property that is binded to the EditText field, so that it holds the value of the number guessed
    When the user presses Submit, create a Command that checks whether the RandomNumber==GuessedValue. If yes, then go to a blank WinPage
    
    **Testing**
    ----------------------------------
    
    * iOS > HomePage > Click Start > enter a number > submit > WinPage
    * Android >  HomePage > Click Start > enter a number > submit > WinPage

---
+++ NumberBomb/NumberBomb/ViewModels/GamePageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace NumberBomb.ViewModels
+{
+    public class GamePageViewModel : INotifyPropertyChanged
+    {
+        public string _number;
+        public int RandomNumber;
+        public int GuessedValue;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand CheckCommand { get; set; }
+
+        public string GuessEntry
+        {
+            get => _number;
+            set
+            {
+                _number = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(GuessEntry)));
+            }
+        }
+
+        public GamePageViewModel()
+        {
+            Random randomNumberGenrator = new Random();
+            RandomNumber = randomNumberGenrator.Next(100) + 1;
+            if (GuessEntry == null)
+            {
+                GuessEntry = "0";
+            }
+            CheckCommand = new Command(CheckCommandExecute);
+        }
+
+        private void CheckCommandExecute(object obj)
+        {
+            GuessedValue = Convert.ToInt32(GuessEntry);
+            if (RandomNumber == GuessedValue)
+            {
+                App.Current.MainPage.Navigation.PushAsync(new WinPage());
+            }
+        }
+    }
+}
