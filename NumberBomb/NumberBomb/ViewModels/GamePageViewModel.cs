8345f31d09e2c7ebbe2c8c12981ed338ae7b268f
Fadoua Amrou <faitamrou.90@gmail.com>
2021-03-09 11:04:12 -0500

    features/35-GamePage-RemainingChancesNLose (#47)
    * features/35-GamePage-RemainingChancesNLose
    
    * Made the "Remain: 10" of the page, come from the string ViewModel property ChancesRemaining
    * If the GuessedNumer != RandomNumber, change the text to "Remain: 9", and keep decreasing
    * If you guess incorrectly when there was only one change
    remaining, go to a blank LosePage
    
    **Testing**
    ------------------------------------
    
    * iOS > HomePage > Click Start > enter a number (right guess) > submit > WinPage
    * Android > HomePage > Click Start > enter a number  (right guess) > submit > WinPage
    * iOS > HomePage > Click Start > enter a number (wrong guess) > submit >  Remain number decreases until uses wins or loses
    * Android > HomePage > Click Start > enter a number (wrong guess) > submit >  Remain number decreases until uses wins or loses
    
    * features/36-GamePage-DescriptionNHint
    
    * Made the description text come from the ViewModel string property Hint
    * If the user guesses incorrectly, the description changes as shown in the design
    * If the GuessedNumber (For eg: 20) < RandomNumber (For eg: 50), then the new lower limit is 20 and the upper limit is 100
    *  If the GuessedNumber (For eg: 80) > RandomNumber (For eg: 50), then the new lower limit is 1 and the upper limit is 80
    
    **Testing**
    -------------------------------------
    
    * iOS > HomePage > Click Start > enter a number (wrong guess) > submit > Remain number decreases > Description changes as shown in the design with the new range
    * Android > HomePage > Click Start > enter a number (wrong guess) > submit > Remain number decreases > Description changes as shown in the design with the new range

--- NumberBomb/NumberBomb/ViewModels/GamePageViewModel.cs
+++ NumberBomb/NumberBomb/ViewModels/GamePageViewModel.cs
@@ -8,10 +8,23 @@ namespace NumberBomb.ViewModels
     public class GamePageViewModel : INotifyPropertyChanged
     {
         public string _number;
+        public string _text;
         public int RandomNumber;
         public int GuessedValue;
+        public int _chances = 10;
+        public int remainNumber;
         public event PropertyChangedEventHandler PropertyChanged;
         public ICommand CheckCommand { get; set; }
+
+        public string Hint
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Hint)));
+            }
+        }
 
         public string GuessEntry
         {
@@ -23,9 +36,20 @@ namespace NumberBomb.ViewModels
             }
         }
 
+         public int ChancesRemaining
+        {
+            get => _chances;
+            set
+            {
+                _chances = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ChancesRemaining)));
+            }
+        }
+
         public GamePageViewModel()
         {
             Random randomNumberGenrator = new Random();
+            _text = "Roboto, you have to find the key to stop the bomb from blowing up. You have to guess a number between 1-100, The correct number will give you the key to defuse the bomb \n But you only have 10 chances to guess it right!";
             RandomNumber = randomNumberGenrator.Next(100) + 1;
             if (GuessEntry == null)
             {
@@ -40,6 +64,30 @@ namespace NumberBomb.ViewModels
             if (RandomNumber == GuessedValue)
             {
                 App.Current.MainPage.Navigation.PushAsync(new WinPage());
+            }
+            else
+            {
+                if(GuessedValue < RandomNumber)
+                {
+                    Hint = "Roboto, " + GuessEntry + " is not the key \n Please enter a number between " + GuessEntry + " - 100";
+                }
+                else
+                {
+                    Hint = "Roboto, " + GuessEntry + " is not the key \n Please enter a number between 1 - " + GuessEntry;
+                }
+
+                if (ChancesRemaining == 1)
+                {
+                    if (RandomNumber != GuessedValue)
+                    {
+                        App.Current.MainPage.Navigation.PushAsync(new LosePage());
+
+                    }
+                }
+                else
+                {
+                    ChancesRemaining -= 1;
+                }               
             }
         }
     }
