fc45d38d1c7d840f1a56f05516e473b2f03a0e2d
Fads09 <faitamrou.90@gmail.com>
2021-03-20 17:11:10 -0400

    features/58-LeaderboardPage-GetAPICall

---
+++ NumberBomb/NumberBomb/Models/ScoreItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace NumberBomb
+{
+    public class ScoreItem
+    {
+        public int Rank { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+    }
+}
---
+++ NumberBomb/NumberBomb/ViewModels/LeaderboardPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NumberBomb.ViewModels
+{
+    public class LeaderboardPageViewModel : INotifyPropertyChanged
+    {
+        public List<ScoreItem> _scoreitems;
+        public List<ScoreItem> ScoreItems {
+                get => _scoreitems;
+                set
+                {
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ScoreItems)));
+                }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LeaderboardPageViewModel()
+        {
+            GetApiCall();
+        }
+
+        private async Task GetApiCall()
+        {
+            var url = "https://script.google.com/macros/s/AKfycbyYixMarhPEUj3UVzOxWne1bJnMZDOcXUn3dHA4UrP21L_qA673bCkeG6Liys7c6oj5PA/exec";
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            ScoreItems = JsonConvert.DeserializeObject<List<ScoreItem>>(result);
+        }
+    }
+}
