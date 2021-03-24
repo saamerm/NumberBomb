8a1cf7a6ec556c4b18928eba1f77a3398fc1fcd1
Fads09 <faitamrou.90@gmail.com>
2021-03-24 15:19:02 -0400

    features/59-LeaderboardPage-PostAPICall
    * Create a function in the WinPage that gets called as soon as the page is loaded
    * This function should make a POST api call as shown with the details below
    * This function is responsible for storing the scores in the database table, no UI change is needed
    
    **Testing**
    ----------------------------------
    
    * Delete app first
    * Win the game with one chance left > Remaining chances is 1, Highest score is 1
    * Win the game with 5 chances left  > Remaining chances is 5, Highest score is 5
    * Win the game with 1 chance left  > Remaining chances is 1, Highest score is 5
    * Play the game > Win the game > Post the data > check the excel sheet > the data is posted

--- NumberBomb/NumberBomb/ViewModels/WinPageViewModel.cs
+++ NumberBomb/NumberBomb/ViewModels/WinPageViewModel.cs
@@ -1,18 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace NumberBomb.ViewModels
 {
     public class WinPageViewModel
     {
+        public int finalScore;
+        public string nameTag;
         public ICommand RestartCommand { get; set; }
         public ICommand MainCommand { get; set; }
 
-        public WinPageViewModel()
+        public WinPageViewModel(int chancesRemaining, string gamerTagName)
         {
+            finalScore = chancesRemaining;
+            nameTag = gamerTagName;
             RestartCommand = new Command(RestartCommandExecute);
             MainCommand = new Command(MainCommandExecute);
+            PostApiCall();
+        }
+
+        private async Task PostApiCall()
+        {
+            var url = "https://script.google.com/macros/s/AKfycbyYixMarhPEUj3UVzOxWne1bJnMZDOcXUn3dHA4UrP21L_qA673bCkeG6Liys7c6oj5PA/exec";
+            var client = new HttpClient();
+            var data = new ScoreItem { Name = nameTag, Score = finalScore };
+            var jsonString = JsonConvert.SerializeObject(data);
+            var requestContent = new StringContent(jsonString);
+            var response = await client.PostAsync(url, requestContent);
+            var result = await response.Content.ReadAsStringAsync();
+            var scoreItems = JsonConvert.DeserializeObject<List<ScoreItem>>(result);
         }
 
         private void RestartCommandExecute(object obj)
