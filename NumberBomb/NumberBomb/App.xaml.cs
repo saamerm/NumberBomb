8ec7e7d10117dc3048bd17d3f5b4ddaf34a9d1a8
Saamer Mansoor <faitamrou.90@gmail.com>
2021-04-01 13:33:50 -0400

    features/iOSnDroid-Appcenter
    * Installed Appcenter, Appcenter crashes, Appcenter Analytics packages for both iOS and Android
    * Initialized AppCenter
    
    **Testing**
    --------------------------
    
    * Build iOS app > use the app > AppCenter > Analytics > see the activity of the user
    * Build Android app > use the app > AppCenter > Analytics > see the activity of the user

--- NumberBomb/NumberBomb/App.xaml.cs
+++ NumberBomb/NumberBomb/App.xaml.cs
@@ -2,6 +2,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Essentials;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace NumberBomb
 {
@@ -26,6 +29,10 @@ namespace NumberBomb
 
         protected override void OnStart()
         {
+            AppCenter.Start("ios=a1028848-3b8c-4c05-b99c-ca2bbf851bf8;" +
+                  "uwp={Your UWP App secret here};" +
+                  "android=0aa22654-3b43-49f1-9d6f-9f7c72c157e8;",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
