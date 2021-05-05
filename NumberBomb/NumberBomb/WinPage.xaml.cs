7b3b4366ddd246152ed0daca2bd32ba253d85480
Fadoua Amrou <faitamrou.90@gmail.com>
2021-05-05 16:23:26 -0400

    features/iOSnDroid-fixes (#79)
    * Changed the UI of the WinPage
    * Added a Confetti to the WinPage
    
    ** Testing **
    ------------------------------------
    
    * Tested on iOS
    * Tested on Android

--- NumberBomb/NumberBomb/WinPage.xaml.cs
+++ NumberBomb/NumberBomb/WinPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using NumberBomb.ViewModels;
 using Xamarin.Forms;
 
@@ -16,7 +17,15 @@ namespace NumberBomb
             _viewModel = new WinPageViewModel(chancesRemaining, gamerTagName);
             BindingContext = _viewModel;     
 
-            label.Text = "Your remaining chances was " + chancesRemaining + " and your highest score is " + score + ".";
+            label.Text = "Your remaining chances was " + chancesRemaining;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await Task.Delay(2000);
+            await AnimationConfetti.FadeTo(1,500);
+            await WinLayout.FadeTo(1, 2000);
         }
     }
 }
