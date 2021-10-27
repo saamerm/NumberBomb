58647128814c2f1fd8df9e872a193a0bdb869c11
Saamer Mansoor <faitamrou.90@gmail.com>
2021-10-26 21:58:44 -0400

    Changed Properties names

--- NumberBomb/NumberBomb/ViewModels/DifficultyViewModel.cs
+++ NumberBomb/NumberBomb/ViewModels/DifficultyViewModel.cs
@@ -10,15 +10,15 @@ namespace NumberBomb.ViewModels
     {
         public ICommand DiffcultyCommand { get; set; }
         public ICommand BackButtonClicked { get; set; }
-        public bool ShouldShow { get; set; }
-        public bool ShouldHide { get; set; }
+        public bool ShouldShowMedium { get; set; }
+        public bool ShouldShowHard { get; set; }
 
         public DifficultyViewModel()
         {
             DiffcultyCommand = new Command<string>(DifficultyCommandExcute);
             BackButtonClicked = new Command(BackButtonClickedCommandExecute);
-            ShouldShow = (Preferences.Get("HighestEasyScore", 0) == 0) ? false : true;
-            ShouldHide = (Preferences.Get("HighestMediumScore", 0) != 0) ? true : false;
+            ShouldShowMedium = (Preferences.Get("HighestEasyScore", 0) == 0) ? false : true;
+            ShouldShowHard = (Preferences.Get("HighestMediumScore", 0) == 0) ? false : true;
         }
 
         private void BackButtonClickedCommandExecute(object obj)
