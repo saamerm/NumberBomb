ad8f93d16fd8b6bf15871f3f70c5c7759e222228
Fadoua Amrou <faitamrou.90@gmail.com>
2021-10-27 11:56:37 -0400

    features/DifficultyLevel-UnlockLevels (#86)
    * features/DifficultyLevel-UnlockLevels
    
    * Hid Medium and Hard buttons on Difficulty page
    * User Should win each level to unlock the next level
    
    **Testing**
    ------------
    
    * Tested on iOS (iPhone 8)
    
    * Changed Properties names
    
    * Removed the converter from the assembly

--- NumberBomb/NumberBomb/ViewModels/DifficultyViewModel.cs
+++ NumberBomb/NumberBomb/ViewModels/DifficultyViewModel.cs
@@ -10,12 +10,15 @@ namespace NumberBomb.ViewModels
     {
         public ICommand DiffcultyCommand { get; set; }
         public ICommand BackButtonClicked { get; set; }
+        public bool ShouldShowMedium { get; set; }
+        public bool ShouldShowHard { get; set; }
 
         public DifficultyViewModel()
         {
             DiffcultyCommand = new Command<string>(DifficultyCommandExcute);
             BackButtonClicked = new Command(BackButtonClickedCommandExecute);
-
+            ShouldShowMedium = (Preferences.Get("HighestEasyScore", 0) == 0) ? false : true;
+            ShouldShowHard = (Preferences.Get("HighestMediumScore", 0) == 0) ? false : true;
         }
 
         private void BackButtonClickedCommandExecute(object obj)
